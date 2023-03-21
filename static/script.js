0f2df2b97ef90597275ac749bf15ac16d9c33c78
Juliane Cassidy <julianecassidy@gmail.com>
2023-03-20 18:37:06 -0600

    add form handler js

--- static/script.js
+++ static/script.js
@@ -3,7 +3,10 @@
 const API_BASE_URL = "/api/cupcakes";
 const $cupcakeList = $("#cupcake-list");
 
-async function getCupcakeList(){
+
+/** Get all cupcakes from the database and display them as list elements */
+
+async function getAndDisplayCupcakeList(){ //could break into 2 pieces
 
   $cupcakeList.empty();
 
@@ -20,5 +23,40 @@ async function getCupcakeList(){
 };
 
 
+/** Create new cupcake from form data, submit a POST request to API, and
+ * append new cupcake to #cupcake-list
+ */
+
+async function addCupcake(event){
+  console.log('addCupcake ran')
+  event.preventDefault();
+
+  const cupcakeDataInput = {
+    flavor: $('#cupcake-flavor').val(),
+    size: $('#cupcake-size').val(),
+    rating: Number($('#cupcake-rating').val()),
+    image: $('#cupcake-image').val()
+  };
+
+  console.log("cupcakeDataInput", cupcakeDataInput);
+
+  const response = await axios ({
+    url: `${API_BASE_URL}`,
+    method: 'POST',
+    data: { // can use cupcakeDataInput; already the object you need
+        flavor: cupcakeDataInput.flavor,
+        size: cupcakeDataInput.size,
+        rating: cupcakeDataInput.rating,
+        image: cupcakeDataInput.image
+    }
+  });
+
+  const {flavor, image} = response.data.cupcake;
+
+  $cupcakeList.append(
+    `<li><img src="${image}" style="max-width: 100px">${flavor}</li>`)
+}
+
+$('#submit').on('click', addCupcake);
 
 getCupcakeList();
